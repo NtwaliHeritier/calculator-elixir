0dfefb35b71f250407a0598d7e455980e81f6d06
NtwaliHeritier <heritierntwali@gmail.com>
2020-09-14 21:40:41 +0200

    completed the calculator

--- calculator/lib/calculator.ex
+++ calculator/lib/calculator.ex
@@ -5,12 +5,40 @@ defmodule Calculator do
 
   defp loop(currentVal) do
     newVal=receive do
-      {:value, client_id} -> send(client_id, {:response, currentVal})
-      {:add, value} -> currentVal+value
+      {:value, client} -> send(client, {:response, currentVal})
+      currentVal
+      {:add, value} -> currentVal + value
       {:substract, value} -> currentVal-value
       {:multiply, value} -> currentVal*value
       {:divide, value} -> currentVal/value
     end
     loop(newVal)
   end
+  
+  def value(server) do
+    send(server, {:value, self()})
+    receive do
+      {:response, value} -> value
+    end
+  end
+
+  def add(server, value) do 
+    send(server, {:add, value})
+    value(server)
+  end
+  
+  def substract(server, value) do 
+    send(server, {:substract, value})
+    value(server)
+  end
+
+  def multiply(server, value) do
+     send(server, {:multiply, value})
+     value(server)
+  end
+  def divide(server, value) do
+     send(server, {:divide, value})
+     value(server)
+  end
 end
+
