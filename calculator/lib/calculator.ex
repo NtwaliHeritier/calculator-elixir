683c0c5545327b13e7746071395a5c12670e8660
NtwaliHeritier <heritierntwali@gmail.com>
2020-09-14 21:12:45 +0200

    completed the basic functions

--- calculator/lib/calculator.ex
+++ calculator/lib/calculator.ex
@@ -1,18 +1,16 @@
 defmodule Calculator do
-  @moduledoc """
-  Documentation for `Calculator`.
-  """
+  def start do
+    spawn(fn->loop(0) end)
+  end
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Calculator.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  defp loop(currentVal) do
+    newVal=receive do
+      {:value, client_id} -> send(client_id, {:response, currentVal})
+      {:add, value} -> currentVal+value
+      {:substract, value} -> currentVal-value
+      {:multiply, value} -> currentVal*value
+      {:divide, value} -> currentVal/value
+    end
+    loop(newVal)
   end
 end
